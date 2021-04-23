476083785e43a0222a2accaa014dd999df6c60e6
Bryan Castro <bry2098@gmail.com>
2021-04-23 17:54:05 -0500

    Preparar repositorio para upload

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import {
+  ActivityIndicator,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Pagos from "./src/components/Pagos";
+import Servicios from "./src/components/Servicios";
+import { Constants } from "./src/constants";
+import { useFonts } from "@use-expo/font";
+import { styles } from "./src/Styles";
 
 export default function App() {
-  return (
+  const [nav, setNav] = useState(1);
+  // require in the font
+  let [fontsLoaded] = useFonts({
+    Nexa: require("./assets/fonts/NexaBold.ttf"),
+  });
+
+  return !fontsLoaded ? (
+    <ActivityIndicator size="large" color={Constants.yellow} />
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" backgroundColor={Constants.yellow} />
+      <View style={styles.headerText}>
+        <Text style={styles.header}>historial</Text>
+      </View>
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.opMenu} onPress={() => setNav(1)}>
+          <Text style={nav == 2 ? styles.txtInactiveMenu : styles.txtMenu}>
+            SERVICIO
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.linea} />
+        <TouchableOpacity style={styles.opMenu} onPress={() => setNav(2)}>
+          <Text style={nav == 1 ? styles.txtInactiveMenu : styles.txtMenu}>
+            Pagos
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {nav == 1 ? <Servicios /> : <Pagos />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
